a80f52719fafa2fbf08029fa7c66d5a743d93b6f
mol211 <victormolins92@gmail.com>
2025-06-05 13:25:52 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Comando para mandar el codigo a Sonar.
+Utilizamos analizador de SonarQube que trabaja con Maven
+Hay que introducir algunos parámetros de configuracion.
+Hay que introducir el puerto, el token de acceso.
+mvn sonar:sonar -Dsonar.login=squ_f4af30e61fbe9672a833298e14e749e1dade7d78 -Dsonar.host.url=http://localhost:8080
---
+++ src/main/java/curso/es/App.java
@@ -0,0 +1,17 @@
+package curso.es;
+
+
+public class App {
+    public static void main( String[] args ) {
+        
+        System.out.println( "Hello World!" );
+
+    }
+
+    public static double sumar(double numero, double numero2) {
+        return numero+numero2;
+    }
+    public static double doblar(double numero){
+        return numero * 2;
+    }
+}

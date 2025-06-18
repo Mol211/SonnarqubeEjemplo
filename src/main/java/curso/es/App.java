8e0cb4a4f7bae3ae758cf2738307bf8d86fe5415
mol211 <victormolins92@gmail.com>
2025-06-18 15:57:52 +0200

    update

--- src/main/java/curso/es/App.java
+++ src/main/java/curso/es/App.java
@@ -2,9 +2,6 @@ package curso.es;
 
 public class App { 
 
-    private App() {
-    throw new IllegalStateException("Utility class");
-    }
     public static double sumar(double numero, double numero2) {
         return numero + numero2;
     }

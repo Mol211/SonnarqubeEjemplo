f2ffb972257d1056a17972b682024c549fc6a121
mol211 <victormolins92@gmail.com>
2025-06-18 13:46:31 +0200

    update Logger

--- src/main/java/curso/es/App.java
+++ src/main/java/curso/es/App.java
@@ -1,17 +1,16 @@
 package curso.es;
-
+import java.util.logging.Logger;
 
 public class App {
+        private static final Logger LOGGER = Logger.getLogger(App.class.getName()); 
     public static void main( String[] args ) {
-        
-        System.out.println( "Hello World!" );
-
+        LOGGER.info("Hello World");
     }
-
     public static double sumar(double numero, double numero2) {
         return numero+numero2;
     }
     public static double doblar(double numero){
         return numero * 2;
     }
+
 }
--- src/test/java/curso/es/AppTest.java
+++ src/test/java/curso/es/AppTest.java
@@ -3,48 +3,59 @@ package curso.es;
 import org.junit.Assert;
 import org.junit.Test;
 
-
 public class AppTest {
-    
+
     @Test
     public void shouldAnswerWithTrue() {
-        Assert.assertTrue( true );
+        Assert.assertTrue(true);
     }
+
     @Test
     public void test_sumar_numeros_positivos() {
-        double devuelto = App.sumar(1,5);
-        Assert.assertEquals( 6.0 , devuelto , 0);
+        double devuelto = App.sumar(1, 5);
+        Assert.assertEquals(6.0, devuelto, 0);
     }
+
     @Test
     public void test_sumar_numeros_negativos() {
-        double devuelto = App.sumar(-1,-5);
-        Assert.assertEquals( -6.0 , devuelto , 0);
+        double devuelto = App.sumar(-1, -5);
+        Assert.assertEquals(-6.0, devuelto, 0);
     }
+
     @Test
     public void test_sumar_numeros_mixtos() {
-        double devuelto = App.sumar(1,-5);
-        Assert.assertEquals( -4.0 , devuelto , 0);
+        double devuelto = App.sumar(1, -5);
+        Assert.assertEquals(-4.0, devuelto, 0);
     }
+
     @Test
     public void test_sumar_numeros_con_cero() {
-        double devuelto = App.sumar(1,0);
-        Assert.assertEquals( 1.0 , devuelto , 0);
+        double devuelto = App.sumar(1, 0);
+        Assert.assertEquals(1.0, devuelto, 0);
     }
-        @Test
+
+    @Test
     public void test_doblar_numeros_positivos() {
         double devuelto = App.doblar(5);
-        Assert.assertEquals( 10.0 , devuelto , 0);
+        Assert.assertEquals(10.0, devuelto, 0);
     }
+
     @Test
     public void test_doblar_numeros_negativos() {
         double devuelto = App.doblar(-1);
-        Assert.assertEquals( -2.0 , devuelto , 0);
+        Assert.assertEquals(-2.0, devuelto, 0);
     }
+
     @Test
     public void test_doblar_cero() {
         double devuelto = App.doblar(0);
-        Assert.assertEquals( 0.0 , devuelto , 0);
+        Assert.assertEquals(0.0, devuelto, 0);
     }
 
-    
+    @Test
+    public void testMain() {
+        App.main(new String[]{});
+        Assert.assertTrue(true); // Solo para que el test tenga un assert válido
+    }
+
 }
